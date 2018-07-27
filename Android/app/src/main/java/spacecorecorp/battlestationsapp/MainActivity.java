4316459a65c37845bdf12b75f98ac80342c5b172
Blaine Huey <bhthenexttesla@gmail.com>
2018-07-26 22:45:10 -0400

    Added UI for Main Activity

--- Android/app/src/main/java/spacecorecorp/battlestationsapp/MainActivity.java
+++ Android/app/src/main/java/spacecorecorp/battlestationsapp/MainActivity.java
@@ -1,15 +1,48 @@
 package spacecorecorp.battlestationsapp;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private EditText editTextName;
+    private EditText editTextIP;
+    private Button buttonProceed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        editTextName = findViewById(R.id.editTextUser);
+        editTextIP = findViewById(R.id.editTextIP);
+        buttonProceed = findViewById(R.id.buttonGo);
+
+        buttonProceed.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(editTextName.getText().toString().equals(""))
+                {
+
+                }
+                else if(editTextIP.getText().toString().equals(""))
+                {
+
+                }
+                else
+                {
+                    
+                }
+            }
+        });
     }
 }
